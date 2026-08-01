65c32076c782ccd15a4db53c35175c89e3c906c0
Gibbs725 <speirssa@gmail.com>
2026-07-31 20:27:15 -0500

    Attached Full EID Data

--- Cattle_Tracker/main.py
+++ Cattle_Tracker/main.py
@@ -5,41 +5,73 @@ import numpy as np
 from sympy.printing.pretty.pretty_symbology import line_width
 from ultralytics import YOLO
 from ultralytics import solutions
+import threading
+#
 
+#Setup Variables
+
+Current_Entry_Track= None
+Current_CHLOW_Track= None
+Current_CHHI_Track= None
+frame_counter = 0
+latest_EID= None
+local_cow=None
 #Reading from the Excel Sheet#
 #pd.read_excel("files/2026-06-26_AnimalUpload.xlsx")
 df=pd.read_excel("files/2026-06-26_AnimalUpload.xlsx",
                  header=1)
 eid_df = df[df["EID*"].notna()] #Filters out "isna" from "notna", obviously
 longID=124000192476682                      #DEBUG'D
-while True:
-    try:
-        eid = 124000192476682 #int(input("Enter EID: "))
-        print("COW: ", eid)
-        Cow_ID = eid_df[eid_df["EID*"].astype(int) == eid]
-        if Cow_ID.empty:
-            print("No EID found")
+
+tracked_animals = {}
+
+
+
+
+#RFID MultiThread
+def rfid_listener():
+    global latest_EID
+    global local_cow
+    while True:
+        try:
+            eid=int(input("Enter EID: "))
+            latest_EID = eid
+            # eid = 124000192476682 #int(input("Enter EID: "))
+            print("COW: ", eid)
+            Cow_ID = eid_df[eid_df["EID*"].astype(int) == eid]
+            if Cow_ID.empty:
+                print("No EID found")
+                continue
+            cow = Cow_ID.iloc[0]
+            local_cow = cow
+            for field, value in cow.items():
+                print(f"{field:<30}: {value}")
             continue
-        cow = Cow_ID.iloc[0]
-        for field, value in cow.items():
-            print(f"{field:<30}: {value}")
-        break
-    except ValueError:
-        print("Invalid EID")
+        except ValueError:
+            print("Invalid EID")
+threading.Thread(target=rfid_listener, daemon=True).start()
+#End RFID MultiThreader
+
+
+#while True:
+#    try:
+#        #eid = 124000192476682 #int(input("Enter EID: "))
+#        #print("COW: ", eid)
+#        Cow_ID = eid_df[eid_df["EID*"].astype(int) == eid]
+#        if Cow_ID.empty:
+#            print("No EID found")
+#            continue
+#        cow = Cow_ID.iloc[0]
+#        for field, value in cow.items():
+#            print(f"{field:<30}: {value}")
+#        break
+#    except ValueError:
+#        print("Invalid EID")
 #End Reading from the Excel Sheet#
 
 #Logging stuff#
 
-tracked_animals = {}
-#tracked_animals[track_id] = {
-#    "EID": cow["EID*"],
-#    "Animal_ID": cow["Animal ID"],
-#    "VID": cow["VID*"],
-#    "Breed": cow["Breed"],
-#    "State": "Identified",
-#    "Location": "TESTPEN"
-#}
-#
+
 
 #Camera 1 initialization#
 Camera = "Camera 1"
@@ -65,6 +97,7 @@ else:
 #    ret2 = False
 
 ######## Pen Regions for Camera 1
+
 
 region_points_1 = {
     "Pen 1": [(0, 0), (0, 100), (640,100), (640, 0)], #TL Corner, BL Corner, TR Corner, BR Corner, from top to bottom, left to right, x0, y0- x640, y480
@@ -94,53 +127,102 @@ Pen_Regions=solutions.RegionCounter(
     verbose= True,
     conf= 0.50)
 
-#Camera
+#Camera Loop
 while cap1.isOpened():
     ret, frame = cap1.read()
     if not ret:
         print("Video frame is empty or processing is complete.")
         break
 
+#Camera/CV Related
+    frame_counter+=1
     results = Pen_Regions(frame)
     #print (results)
     #print (results.total_tracks)
     #print (results.region_counts)
     #print (dir(results))
-    track_results= model.track(frame, persist=True, classes=[0])
-    track_ids = track_results[0].boxes[0].id
+    track_results= model.track(frame, persist=True, classes=[0]) #0 is Human
+    tracks_to_remove=[]
 
-    for track_id in track_ids:
-        track_id = int(track_id.item())
+#Checks for the Global RFID input
+    if latest_EID is not None:
+        print("New EID: ", latest_EID)
 
-        if track_id not in tracked_animals:
-            tracked_animals[track_id] = {
-                "State": "Unknown",
-                "Location": "Unknown",
-            }
-        else: tracked_animals[track_id]["State"] = "Detected"
-        # print(track_id)
+#Protects against "No Detect" issues
+    if len(track_results)>0:
+        track_ids = track_results[0].boxes.id
+        if track_ids is not None:
+            for track_id in track_ids:
+                track_id = int(track_id.item())
+
+#Creates New Track ID
+                if track_id not in tracked_animals:
+                    tracked_animals[track_id] = {
+                        "State": "Unknown",
+                        "Location": "Unknown",
+                        "LastSeen": frame_counter,
+                    }
+#Sets "Freshness"
+                tracked_animals[track_id]["LastSeen"] = frame_counter
+#Cleanup old tracks
+                for old_track in tracked_animals:
+                    if frame_counter - tracked_animals[old_track]["LastSeen"] > 30:
+                        tracks_to_remove.append(old_track)
+                for old_track in tracks_to_remove:
+                    print("deleting track: ", old_track)
+                    del tracked_animals[old_track]
+                    if old_track== Current_Entry_Track:
+                        Current_Entry_Track= None
+#End Cleanup
+#Sets Default
+                else:
+                    if tracked_animals[track_id]["State"] == "Unknown":
+                        tracked_animals[track_id]["State"] = "Detected"
+#Event Transitions (for TESTPEN)
+                current_testpen = results.region_counts["TESTPEN"]
+                if current_testpen > 1:
+                    print("MultipleCows")
+                else:
+                    if previous_testpen == 0 and current_testpen == 1:
+                        print("Track ID ", track_id, "Entered TESTPEN")
+                        Current_Entry_Track= track_id
+                        tracked_animals[track_id]["Location"] = "TESTPEN"
+                    if previous_testpen == 1 and current_testpen == 0:
+                        print("Track ID ", track_id, "Exited TESTPEN")
+                        Current_Entry_Track= None
+                        tracked_animals[track_id]["Location"] = "Unknown"
+                previous_testpen = current_testpen
+#Event Transitions (for TESTPEN) end
+#"Processing" EID and Tracking Assignment
+                if latest_EID is not None and Current_Entry_Track is not None:
+                    tracked_animals[Current_Entry_Track]["EID"] = latest_EID
+                    tracked_animals[Current_Entry_Track]["VID"] = local_cow["VID*"]
+                    tracked_animals[Current_Entry_Track]["AnimalID"] = local_cow["Animal ID"]
+                    tracked_animals[Current_Entry_Track]["Breed"]= local_cow["Breed"]
+                    tracked_animals[Current_Entry_Track]["State"] = "Classified"
+                    print(
+                        f"Attached EID: {latest_EID}"
+                        f"To Track ID: {Current_Entry_Track}"
+                    )
+                    latest_EID = None
+#Clears the Global EID ready for the next scan input
+                print(track_id)
+                print(tracked_animals)
+                print("frame: ", frame_counter)
+    else:
+        pass
+#Escape
+    cv2.imshow(Camera, frame)
     # tracked_animals[track_id] = animal_record
     # print(track_results[0].boxes.id)
     #print(track_results[0].boxes.cls)
     #print(track_results[0].boxes.conf)
-    current_testpen=results.region_counts["TESTPEN"]
-    if current_testpen > 1:
-        print("MultipleCows")
-    else:
-        if previous_testpen == 0 and current_testpen == 1:
-            print("Track ID ", track_id, "Entered TESTPEN")
-            tracked_animals[track_id]["Location"] = "TESTPEN"
-        if previous_testpen == 1 and current_testpen == 0:
-            print("Track ID ", track_id, "Exited TESTPEN")
-            tracked_animals[track_id]["Location"] = "Unknown"
-    previous_testpen=current_testpen
+
     #print(dir(track_results[0]))
-    g, h, i, j = results.region_counts.values()
+    #g, h, i, j = results.region_counts.values()
     #print("Pen_1", g, "Pen_2", h, "Pen_3", i, "TestPen", j)
     #print("Animals Currently In TESTPEN ", track_id)
-    print(tracked_animals)
 
-    cv2.imshow(Camera, frame)
 
     #ret2, frame2 = cap2.read()
     #cv2.imshow(cap2, frame2)
@@ -148,16 +230,8 @@ while cap1.isOpened():
     #print(results.total_tracks, results.region_counts.values())
     #for (x, y, z) in results.region_counts.values():
         #print(x,y,z)
-
-
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
-
 cap1.release()
 #cap2.release()
 cv2.destroyAllWindows()
-
-
-
-
-
